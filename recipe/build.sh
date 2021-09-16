14c285af5f3af24ea5acb941de2e10087870c4fb
Jan-Benedikt Jagusch <jan.jagusch@gmail.com>
2021-09-16 19:25:13 +0300

    make build work on osx arm

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,13 @@
+#!/usr/bin/env bash
+
 yarn pack
 yarn licenses generate-disclaimer > ThirdPartyLicenses.txt
 NPM_CONFIG_USERCONFIG=/tmp/nonexistentrc
+
+echo "$target_platform"
+if [ "$target_platform" == "osx-arm64" ]; then
+    export npm_config_arch="x64"
+fi
+
+target_platform
 npm install -g ${PKG_NAME}-v${PKG_VERSION}.tgz
