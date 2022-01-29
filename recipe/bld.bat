fbe638dff7f841c6c96f6a2cd06c89ef77c90963
Jan-Benedikt Jagusch <jan.jagusch@gmail.com>
2022-01-29 22:05:35 +0100

    remove verbose loglevel

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,7 +2,7 @@ call yarn pack || goto :error
 call yarn licenses generate-disclaimer > ThirdPartyLicenses.txt || goto :error
 
 call npm config set prefix %BUILD_PREFIX% || goto :error
-call npm install --global --logLevel verbose %PKG_NAME%-v%PKG_VERSION%.tgz || goto :error
+call npm install --global %PKG_NAME%-v%PKG_VERSION%.tgz || goto :error
 
 :error
 echo Failed with error #%errorlevel%.
