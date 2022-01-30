e700cab00679e523232d9ead8a575a99f39a13ce
Jan-Benedikt Jagusch <jan.jagusch@gmail.com>
2022-01-30 10:16:32 +0100

    clean up unix build

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,15 +1,13 @@
 #!/usr/bin/env bash
 
 set -euxo pipefail
+
 if [[ "${target_platform}" == "osx-arm64" ]]; then
     export npm_config_arch="arm64"
 fi
 
-rm $PREFIX/bin/node
-ln -s $BUILD_PREFIX/bin/node $PREFIX/bin/node
-
 yarn pack
 yarn licenses generate-disclaimer > ThirdPartyLicenses.txt
-NPM_CONFIG_USERCONFIG=/tmp/nonexistentrc
 
-npm install -g ${PKG_NAME}-v${PKG_VERSION}.tgz
+npm config set prefix "${BUILD_PREFIX}"
+npm install --global "${PKG_NAME}-v${PKG_VERSION}.tgz"
