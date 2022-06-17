6af2dbf50d94d085ce018a7773d24c06c9cfd485
Uwe L. Korn <uwe.korn@quantco.com>
2022-06-17 19:20:26 +0200

    Rewrite bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,11 +1,23 @@
 @echo on
-call yarn pack || goto :error
-call yarn licenses generate-disclaimer > ThirdPartyLicenses.txt || goto :error
+cmd /c "yarn pack"
+if errorlevel 1 exit 1
+cmd /c "yarn licenses generate-disclaimer > ThirdPartyLicenses.txt"
+if errorlevel 1 exit 1
 
-@echo on
-where npm
-call npm install --userconfig nonexistentrc --global %PKG_NAME%-v%PKG_VERSION%.tgz || goto :error
+md %LIBRARY_PREFIX%\share\prettier
+pushd %LIBRARY_PREFIX%\share\prettier
 
-:error
-echo Failed with error #%errorlevel%.
-exit /b %errorlevel%
+md node_modules
+cmd /c "npm install prettier@%PKG_VERSION%"
+if errorlevel 1 exit 1
+
+dir node_modules
+dir node_modules\.bin
+dir node_modules\prettier
+dir node_modules\prettier\bin
+
+pushd %LIBRARY_PREFIX%\bin
+for %%c in (prettier) do (
+  echo @echo on >> %%c.bat
+  echo %LIBRARY_PREFIX%\share\prettier\node_modules\.bin\%%c.cmd "%%*" >> %%c.bat
+)
