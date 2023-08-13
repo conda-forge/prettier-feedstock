0febd917f69a235fcab3096207bbc1bbbaacba0b
Jannik Wibker <jannikwibker@gmail.com>
2023-08-13 16:56:11 +0200

    update prettier to 3.0.1 and switch to pnpm/pnpm-licenses for ThirdPartyLicenses

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,18 +1,26 @@
 @echo on
-cmd /c "yarn pack"
-if errorlevel 1 exit 1
-cmd /c "yarn licenses generate-disclaimer > ThirdPartyLicenses.txt"
-if errorlevel 1 exit 1
 
 md %LIBRARY_PREFIX%\share\prettier
 pushd %LIBRARY_PREFIX%\share\prettier
-
 md node_modules
 cmd /c "npm install prettier@%PKG_VERSION%"
 if errorlevel 1 exit 1
+popd
 
 pushd %LIBRARY_PREFIX%\bin
 for %%c in (prettier) do (
   echo @echo on >> %%c.bat
   echo "%LIBRARY_PREFIX%\share\prettier\node_modules\.bin\%%c.cmd" %%* >> %%c.bat
 )
+popd
+
+@rem port yarn.lock to pnpm-lock.yaml
+npx pnpm@latest import
+
+@rem install all (prod) dependencies, this needs to be done for pnpm to properly list all dependencies later on
+npx pnpm@latest install --prod
+if errorlevel 1 exit 1
+
+@rem list all dependencies and then call pnpm-licenses to generate the ThirdPartyLicenses.txt file
+npx pnpm@latest licenses list --prod --json | npx @quantco/pnpm-licenses generate-disclaimer --prod --json-input --output-file=ThirdPartyLicenses.txt
+if errorlevel 1 exit 1
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,15 +1,23 @@
 #!/usr/bin/env bash
 
 set -euxo pipefail
+
 if [[ "${target_platform}" == "osx-arm64" ]]; then
     export npm_config_arch="arm64"
 fi
 
+# Don't use pre-built gyp packages
+export npm_config_build_from_source=true
+
 rm $PREFIX/bin/node
 ln -s $BUILD_PREFIX/bin/node $PREFIX/bin/node
 
-yarn pack
-yarn licenses generate-disclaimer > ThirdPartyLicenses.txt
 NPM_CONFIG_USERCONFIG=/tmp/nonexistentrc
 
-npm install -g ${PKG_NAME}-v${PKG_VERSION}.tgz
+# install prettier globally from the npm registry
+# all things coming after this are just concerned with generating the ThirdPartyLicenses.txt file
+npm install -g ${PKG_NAME}@${PKG_VERSION}
+
+npx pnpm@latest import
+npx pnpm@latest install --prod
+npx pnpm@latest licenses list --json | npx @quantco/pnpm-licenses generate-disclaimer --json-input --output-file=ThirdPartyLicenses.txt
